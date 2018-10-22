8e69d7d9b363b911a717a63ea6bf61709b91b14d
Rohan Garg <rohgarg@ccs.neu.edu>
2018-10-22 09:02:02 -0400

    target: Minor reorg

--- target.c
+++ target.c
@@ -4,17 +4,17 @@
 #include <cuda.h>
 #include "upper-half-cuda-wrappers.h"
 
-int main(int argc, char **argv)
+static void processArgs(int, const char** );
+
+int
+main(int argc, char **argv)
 {
   int i = 0;
-  if (argc > 1) {
-    printf("Application was called with the following args: ");
-    for (int j = 1; j < argc; j++) {
-      printf("%s ", argv[j]);
-    }
-    printf("\n");
-  }
+
+  processArgs(argc, argv);
+
   printf("cuInit returned: %d\n", (int)cuInit(0));
+
   while (1) {
     printf("%d ", i);
     fflush(stdout);
@@ -22,4 +22,16 @@ int main(int argc, char **argv)
     i++;
   }
   return 0;
+}
+
+static void
+processArgs(int argc, const char** argv)
+{
+  if (argc > 1) {
+    printf("Application was called with the following args: ");
+    for (int j = 1; j < argc; j++) {
+      printf("%s ", argv[j]);
+    }
+    printf("\n");
+  }
 }
