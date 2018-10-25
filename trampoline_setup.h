9013e4256a923ac2e8bc186ab62169f0b15be514
Twinkle Jain <jain.t@husky.neu.edu>
2018-10-25 17:06:18 -0400

    fixup! Add code to patch upper-half's libc

---
+++ trampoline_setup.h
@@ -0,0 +1,7 @@
+#ifndef TRAMPOLINE_SETUP_H
+#define TRAMPOLINE_SETUP_H
+
+int insertTrampoline(void* , void* );
+off_t get_symbol_offset(int , const char* , const char* );
+
+#endif // ifndef TRAMPOLINE_SETUP_H
